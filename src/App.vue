5da7d11235bf783687079518292d8e4f91a00c75
bkLinux <xiaoj655@gmail.com>
2023-05-12 12:21:18 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,337 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div class="wrapper">
+      <div class="position">
+        <span class="iconfont position__icon">&#xe7f1;</span>
+        深圳技术大学深圳技术大学深圳技术大学深圳技术大学深圳技术大学深圳技术大学
+        <span class="iconfont position__notice">&#xe60d;</span>
+        <div class="search">
+          <span class="iconfont">&#xe6e1;</span>
+          <span class="search__text">山姆会员商店优惠商品</span>
+        </div>
+      </div>
+      <div class="banner">
+          <img class="banner__img" src="http://www.dell-lee.com/imgs/vue3/banner.jpg">
+        </div>
+        <div class="icons">
+          <div class="icons__item">
+            <img src="https://m.360buyimg.com/babel/jfs/t1/57520/8/375/4092/5cd4f1d8Ea1266047/1c590322ece537ba.png" class="icons__item__img" />
+            <p class="icons__item__desc">超市便利</p>
+          </div>
+          <div class="icons__item">
+            <img src="https://m.360buyimg.com/babel/jfs/t1/57520/8/375/4092/5cd4f1d8Ea1266047/1c590322ece537ba.png" class="icons__item__img" />
+            <p class="icons__item__desc">超市便利</p>
+          </div>
+          <div class="icons__item">
+            <img src="https://m.360buyimg.com/babel/jfs/t1/57520/8/375/4092/5cd4f1d8Ea1266047/1c590322ece537ba.png" class="icons__item__img" />
+            <p class="icons__item__desc">超市便利</p>
+          </div>
+          <div class="icons__item">
+            <img src="https://m.360buyimg.com/babel/jfs/t1/57520/8/375/4092/5cd4f1d8Ea1266047/1c590322ece537ba.png" class="icons__item__img" />
+            <p class="icons__item__desc">超市便利</p>
+          </div>
+          <div class="icons__item">
+            <img src="https://m.360buyimg.com/babel/jfs/t1/57520/8/375/4092/5cd4f1d8Ea1266047/1c590322ece537ba.png" class="icons__item__img" />
+            <p class="icons__item__desc">超市便利</p>
+          </div>
+          <div class="icons__item">
+            <img src="https://m.360buyimg.com/babel/jfs/t1/57520/8/375/4092/5cd4f1d8Ea1266047/1c590322ece537ba.png" class="icons__item__img" />
+            <p class="icons__item__desc">超市便利</p>
+          </div>
+          <div class="icons__item">
+            <img src="https://m.360buyimg.com/babel/jfs/t1/57520/8/375/4092/5cd4f1d8Ea1266047/1c590322ece537ba.png" class="icons__item__img" />
+            <p class="icons__item__desc">超市便利</p>
+          </div>
+          <div class="icons__item">
+            <img src="https://m.360buyimg.com/babel/jfs/t1/57520/8/375/4092/5cd4f1d8Ea1266047/1c590322ece537ba.png" class="icons__item__img" />
+            <p class="icons__item__desc">超市便利</p>
+          </div>
+          <div class="icons__item">
+            <img src="https://m.360buyimg.com/babel/jfs/t1/57520/8/375/4092/5cd4f1d8Ea1266047/1c590322ece537ba.png" class="icons__item__img" />
+            <p class="icons__item__desc">超市便利</p>
+          </div>
+          <div class="icons__item">
+            <img src="https://m.360buyimg.com/babel/jfs/t1/57520/8/375/4092/5cd4f1d8Ea1266047/1c590322ece537ba.png" class="icons__item__img" />
+            <p class="icons__item__desc">超市便利</p>
+          </div>
+        </div>
+        <div class="gap"></div>
+        <div class="nearby">
+          <h3 class="nearby__title">附近店铺</h3>
+          <div class="nearby__item">
+            <img src="https://i5.walmartimages.com/dfw/63fd9f59-ac39/29c6759d-7f14-49fa-bd3a-b870eb4fb8fb/v1/wplus-icon-blue.svg" class="nearby__item__img">
+            <div class="nearby__content">
+              <div class="nearby__content__title">沃尔玛</div>
+              <div class="nearby__content__tags">
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+              </div>
+              <p class="nearby__content__highlight">VIP尊享满89元减4元运费券(每月3张)</p>
+            </div>
+          </div>
+          <div class="nearby__item">
+            <img src="https://i5.walmartimages.com/dfw/63fd9f59-ac39/29c6759d-7f14-49fa-bd3a-b870eb4fb8fb/v1/wplus-icon-blue.svg" class="nearby__item__img">
+            <div class="nearby__content">
+              <div class="nearby__content__title">沃尔玛</div>
+              <div class="nearby__content__tags">
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+              </div>
+              <p class="nearby__content__highlight">VIP尊享满89元减4元运费券(每月3张)</p>
+            </div>
+          </div>
+          <div class="nearby__item">
+            <img src="https://i5.walmartimages.com/dfw/63fd9f59-ac39/29c6759d-7f14-49fa-bd3a-b870eb4fb8fb/v1/wplus-icon-blue.svg" class="nearby__item__img">
+            <div class="nearby__content">
+              <div class="nearby__content__title">沃尔玛</div>
+              <div class="nearby__content__tags">
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+              </div>
+              <p class="nearby__content__highlight">VIP尊享满89元减4元运费券(每月3张)</p>
+            </div>
+          </div>
+          <div class="nearby__item">
+            <img src="https://i5.walmartimages.com/dfw/63fd9f59-ac39/29c6759d-7f14-49fa-bd3a-b870eb4fb8fb/v1/wplus-icon-blue.svg" class="nearby__item__img">
+            <div class="nearby__content">
+              <div class="nearby__content__title">沃尔玛</div>
+              <div class="nearby__content__tags">
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+              </div>
+              <p class="nearby__content__highlight">VIP尊享满89元减4元运费券(每月3张)</p>
+            </div>
+          </div>
+          <div class="nearby__item">
+            <img src="https://i5.walmartimages.com/dfw/63fd9f59-ac39/29c6759d-7f14-49fa-bd3a-b870eb4fb8fb/v1/wplus-icon-blue.svg" class="nearby__item__img">
+            <div class="nearby__content">
+              <div class="nearby__content__title">沃尔玛</div>
+              <div class="nearby__content__tags">
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+                <span class="nearby__content__tag">
+                  月售1万+
+                </span>
+              </div>
+              <p class="nearby__content__highlight">VIP尊享满89元减4元运费券(每月3张)</p>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+  <div class="docker">
+    <span class="docker__item docker__item--active">
+      <div class="iconfont">&#xe7c6;</div>
+      <div class="docker__title">首页</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe698;</div>
+      <div class="docker__title">购物车</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe600;</div>
+      <div class="docker__title">订单</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe78b;</div>
+      <div class="docker__title">我的</div>
+    </span>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import './style/virables.scss';
+@import './style/mixins.scss';
+
+.docker{
+  display: flex;
+  position: absolute;
+  box-sizing: border-box;
+  padding: 0 .18rem;
+  left: 0;
+  bottom: 0;
+  width:100%;
+  height: 0.49rem;
+  border-top: .01rem solid #f1f1f1;
+  color: $content-fontcolor;
+  &__item {
+  flex:1;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  .iconfont{
+    margin-top: .07rem;
+    margin-bottom: .018rem;
+    font-size: .2rem;
+  }
+  &--active{
+  color: #1FA4FC;
     }
   }
+}
+
+.docker__title{
+  font-size: 20px;
+  transform: scale(0.5,0.5);
+  transform-origin: center top;
+}
+.wrapper {
+  overflow-y: auto;
+  overflow-x: hidden;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: .5rem;
+  top: 0;
+  padding: 0 .18rem .2rem .18rem;
+  .position {
+    position: relative;
+    padding: .16rem 0;
+    padding-right: .16rem;
+    line-height: .22rem;
+    font-size: .16rem;
+    @include ellipsis;
+    .position__icon {
+      position: relative;
+      top: .01rem;
+      font-size: .2rem;
+    }
+    .position__notice {
+      position: absolute;
+      right: 0;
+      top: .17rem;
+      font-size: .2rem;
+    }
+    color: $content-fontcolor;
+  }
+}
+.search {
+  margin: .16rem .18rem .12rem .18rem;
+  background-color: #f5f5f5;
+  border-radius: .16rem ;
+  height: .32rem;
+  width: 339px;
+  line-height: .32rem;
+  .iconfont{
+    font-size: .18rem;
+    margin-left: .16rem;
+  }
+  &__text{
+    position: relative;
+    top: -.02rem;
+    margin-left: .12rem;
+    font-size: .15rem;
+    color: #b7b7b7;
+  }
+}
+.banner{
+  // width: 3.39rem;
+  // height: .86rem;
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  margin:0 .18rem .16rem .18rem ;
+  &__img{
+    width: 100%;
+  }
+}
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: .16rem;
+  &__item{
+    width: 20%;
+    &__img{
+      display: block;
+      width: .4rem;
+      height: .4rem;
+      margin: 0 auto;
+    }
+    &__desc{
+      margin: .05rem 0 .16rem 0;
+      text-align: center;
+      color: $content-fontcolor;
+    }
+  }
+}
+.gap {
+  overflow: visible;
+  margin: 0 -.3rem;
+  height: .1rem;
+  background: #f1f1f1;
+}
+.nearby {
+  &__title {
+    margin: .16rem 0 .02rem 0;
+    font-size: .18rem;
+    font-weight: normal;
+    color: $content-fontcolor;
+  }
+  &__item {
+    display: flex;
+    padding-top: .12rem;
+    &__img {
+      margin-right: .16rem;
+      width: .56rem;
+      height: .56rem;
+    }
+  }
+  &__content {
+    flex:1;
+    padding-bottom: .12rem;
+    border-bottom: 1px solid #f1f1f1;
+    &__title {
+      line-height: .22rem;
+      font-size: .16rem;
+      color: $content-fontcolor;
+    }
+    &__tags{
+      line-height: .18rem;
+      font-size: .13rem;
+      color: $content-fontcolor;
+    }
+    &__tag {
+      margin-right: .16rem;
+    }
+    &__highlight {
+      margin-top: .08rem 0 0 0;
+      line-height: .18rem;
+      font-size: .13rem;
+      color: #E93B3B;
+    }
+  }
+}
+body{
+  font-size: .12rem;
 }
 </style>
